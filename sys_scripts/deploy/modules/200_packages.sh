b0463414175a5dd72bc8c6dd3f7ac8de34bca362
James Bair <james.d.bair@gmail.com>
2010-11-15 14:58:21 -0600

    Updated the packages to add a few more, swapped out slocate for mlocate.

--- sys_scripts/deploy/modules/200_packages.sh
+++ sys_scripts/deploy/modules/200_packages.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Installs the following system packages. None of these 
 # should require any configuration.
-packages='host iftop iptraf netcat nmap screen slocate telnet-bsd whois'
+packages='ftp host iftop iptraf lsof mlocate netcat nmap rar rdiff-backup 
+          screen strace tcpdump telnet-bsd unrar unzip whois zip'
+
+# Install our packages if needed.
 emerge -u ${packages} || exit 1
-echo "Running updatedb for slocate."
+
+# The only client package we should be "configuring"
+echo -n "Running updatedb for mlocate..."
 updatedb || exit 1
+echo 'done.'
+
+# All done.
 exit 0
