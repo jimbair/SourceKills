1410857757c15c4ca7c45f7d4bbad55c29800639
James Bair <james.d.bair@gmail.com>
2010-11-15 15:49:55 -0600

    Swapped order on symlink.

--- sys_scripts/deploy/modules/202_host.sh
+++ sys_scripts/deploy/modules/202_host.sh
@@ -10,7 +10,7 @@ emerge -u host || exit 1
 echo -n "Creating our symlink..."
 cd $(dirname $(which host)) || exit 1
 # Only create symlink if nothing exists
-[ ! -e host ] && ln -s hostx host || exit 1
+[ ! -e host ] && ln -s host hostx || exit 1
 cd ${back} || exit 1
 echo 'done.'
 
