471916374cd23fe1ddb17a0168433424a1d781e7
James Bair <james.d.bair@gmail.com>
2010-11-17 18:16:46 -0600

    Some more sanity checking added to vnstat module.

--- sys_scripts/deploy/modules/201_vnstat.sh
+++ sys_scripts/deploy/modules/201_vnstat.sh
@@ -3,6 +3,11 @@
 
 # Add more interfaces if you want here.
 interfaces='eth0'
+
+# Stuff that should stay constant in Gentoo. Change if needed.
+dname='vnstatd'
+initScript="/etc/init.d/${dname}"
+runlevel='default'
 user='vnstat'
 vnstatDir='/var/lib/vnstat'
 
@@ -14,6 +19,7 @@ for file in passwd group; do
     grep "${user}" "/etc/${file}" &>/dev/null || exit 1
 done
 
+# Setup our interfaces if needed.
 for int in ${interfaces}; do 
     # Create new DB and chown to vnstat user
     # if it's not there.
@@ -29,7 +35,27 @@ for int in ${interfaces}; do
 done
 
 # Start it up and set to startup
-/etc/init.d/vnstatd start || exit 1
-rc-update add vnstatd default || exit 1
+if [ ! -s "${initScript}" -o ! -x "${initScript}" ]; then
+    echo "Init script is empty or not executable."
+    exit 1
+fi
 
+# Find our status and start if needed
+ourStatus="$(${initScript} status | awk '{print $NF}')"
+if [ "${ourStatus}" != 'started' ]; then
+    ${initScript} start || exit 1
+else
+    echo "${dname} is already running."
+fi
+
+# Find our runlevel and add if needed
+currentRunlevel="$(rc-update -s | grep "${dname}" | awk '{print $NF}')"
+if [ "${currentRunlevel}" != "${runlevel}" ]; then
+    echo "Setting runlevel for ${dname} to ${runlevel}."
+    rc-update add ${dname} ${runlevel} || exit 1
+else
+    echo "Runlevel for ${dname} already set to ${currentRunlevel}."
+fi
+
+# All done.
 exit 0
