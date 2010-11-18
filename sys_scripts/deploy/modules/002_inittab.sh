e1845193d20f83c90f7a0d3bb68e02d4ec1347cf
James Bair <james.d.bair@gmail.com>
2010-11-18 14:16:04 -0600

    Adding a module for configuring inittab.

---
+++ sys_scripts/deploy/modules/002_inittab.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This module configures our inittab.
+# This fixes the respawning errors in syslog. 
+# Pretty sure this is specific to our Xen slices
+
+ourFile='/etc/inittab'
+string='cX:12345:respawn:/sbin/agetty 38400 ttyX linux'
+
+# Make sure our file exists
+echo "Checking if ${ourFile} exists."
+[ -s "${ourFile}" ] || exit 1
+
+# Patch our file as needed.
+for X in $(seq 6); do
+    # Build the string we need to patch
+    newString="$(echo ${ourString} | sed s/X/${X}/g)"
+    [ -z "${newString}" ] && echo "Can't build our new string" && exit 1
+    echo "Working on $newString."
+
+    # Make sure it needs patched
+    echo "Checking if we need to patch ${ourFile}."
+    egrep "^${newString}$" ${ourFile} || continue
+
+    # Patch it if we're still here
+    echo -n "Patching..."
+    sed -i "s/${newString}/#${newString}/g" || exit 1
+    echo 'done.'
+
+done
+
+# All done
+exit 0
