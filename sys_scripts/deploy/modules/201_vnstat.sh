61754b40358c18b22b33c5ea20c9c09a18c57d6d
James Bair <james.d.bair@gmail.com>
2010-11-17 18:04:30 -0600

    Made lots of changes to vnstat module.

--- sys_scripts/deploy/modules/201_vnstat.sh
+++ sys_scripts/deploy/modules/201_vnstat.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
-# Script to setup vnstats for eth0
+# Script to setup vnstats for the interfaces specified.
 
-# Install the app
-emerge vnstat || exit 1
+# Add more interfaces if you want here.
+interfaces='eth0'
+user='vnstat'
+vnstatDir='/var/lib/vnstat'
 
-# Create new DB and chown to vnstat user
-vnstat -u -i eth0
-chown -R vnstat:vnstat /var/lib/vnstat
+# Install the app if needed
+emerge -u vnstat || exit 1
+
+# Make sure our vnstat user exists
+for file in passwd group; do
+    grep "${user}" "/etc/${file}" &>/dev/null || exit 1
+done
+
+for int in ${interfaces}; do 
+    # Create new DB and chown to vnstat user
+    # if it's not there.
+    dbFile="${vnstatDir}/${int}"
+    if [ ! -s "${dbFile}" ]; then
+        echo "Configuring vnstat with ${int}"
+        vnstat -u -i ${int} || exit 1
+        chown -R "${user}:${user}" /var/lib/vnstat || exit 1
+        echo "Configured ${int} successfully."
+    else
+        echo "Skipping ${int} - already configured."
+    fi
+done
 
 # Start it up and set to startup
 /etc/init.d/vnstatd start || exit 1
