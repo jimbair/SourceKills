199293bb3c7b3d1fcd723aac8d2bbb5408b37f98
James Bair <james.d.bair@gmail.com>
2010-11-25 22:55:00 -0600

    Trying to make this a little better.

--- sys_scripts/deploy/modules/001_locale.gen.sh
+++ sys_scripts/deploy/modules/001_locale.gen.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # Patch locale.gen to only do stuff for en_US
 ourFile='/etc/locale.gen'
+string1='en_US ISO-8859-1'
+string2='en_US.UTF-8 UTF-8'
 if [ ! -s "${ourFile}" ]; then
     echo "${ourFile} is missing." >&2
     exit 1
 fi
 
-sed -i 's/#en_US ISO-8859-1/en_US ISO-8859-1/' ${ourFile}
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' ${ourFile}
-
+# Only patches if we need to
+for string in "${string1}" "${string2}"; do
+    if [ -n "$(egrep "^#${string}" "${ourFile}")" ]; then
+        echo -n "Patching for ${string}..."
+        sed -i "s/#${string}/${string}/" ${ourFile}
+        echo "done."
+    else
+        echo "Skipping ${string} - already patched."
+    fi
+done
 exit 0
