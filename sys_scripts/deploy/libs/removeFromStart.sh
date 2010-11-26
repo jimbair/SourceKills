34f3f9b1476201aa497965ddf3afda2496b79855
James Bair <james.d.bair@gmail.com>
2010-11-26 17:42:17 -0600

    Safer check for skipping. Also throws error if user in boot runlevel. Need to accept  for optional runlevel.

--- sys_scripts/deploy/libs/removeFromStart.sh
+++ sys_scripts/deploy/libs/removeFromStart.sh
@@ -10,8 +10,14 @@ removeFromStart() {
     if [ "${currentRunlevel}" == "${runlevel}" ]; then
         echo "Removing ${service} service from ${runlevel} runlevel."
         rc-update del ${service} ${runlevel} || exit 1
+    elif [ -z "${currentRunlevel}" ]; then
+        echo "Service ${service} not set to any runlevel - skipping"
     else
-        echo "Service ${service} not set to ${currentRunlevel} runlevel."
+        echo "Unexpected result - please debug." >&2
+        echo "service: ${service}" >&2
+        echo "current runlevel: ${currentRunlevel}" >&2
+        echo "expected runlevel: ${runlevel}" >&2
+        exit 1
     fi
 
 }
