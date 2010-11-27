8bf7aa758d6113b5f21d822ffe4e2d2bf4cada2d
James Bair <james.d.bair@gmail.com>
2010-11-26 22:30:08 -0600

    Bugfix on host module.

--- sys_scripts/deploy/modules/202_host.sh
+++ sys_scripts/deploy/modules/202_host.sh
@@ -6,12 +6,22 @@ back="$(pwd)"
 # Install our package if needed.
 emerge -u host || exit 1
 
+ourFolder="$(dirname $(which host))"
+if [ -z "${ourFolder}" ]; then
+    echo "Cannot find folder for host." >&2
+    exit 1
+fi
+
 # Setup the symlink if needed
 echo -n "Creating our symlink..."
-cd $(dirname $(which host)) || exit 1
-# Only create symlink if nothing exists
+cd "${ourFolder}" || exit 1
 [ ! -e host ] && ln -s hostx host || exit 1
 cd ${back} || exit 1
+echo 'done.'
+
+# Make sure symlink works
+echo -n "Validating symlink..."
+[ "$(which host)" ] || exit 1
 echo 'done.'
 
 # All done.
