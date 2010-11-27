b5818f021f6fce205641abc1483fab13fd3815e1
James Bair <james.d.bair@gmail.com>
2010-11-27 09:27:27 -0600

    Bugfixes and some extra functionality.

--- sys_scripts/deploy/modules/400_sshd_config.sh
+++ sys_scripts/deploy/modules/400_sshd_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This module tweaks the sshd config to our needs.
 ourFile='/etc/ssh/sshd_config'
+updated='false'
 checkForFile ${ourFile} || exit 1
 
 # If we don't have an SSH port, set it to the default
@@ -8,28 +9,48 @@ checkForFile ${ourFile} || exit 1
 
 # See which way we need to patch our config
 portType='dead'
-livePort=$(egrep "^Port ${sshPort}" ${ourFile} | awk '{print $2}')
+livePort=$(egrep "^Port [0-9]" "${ourFile}" | awk '{print $2}')
 if [ -n "${livePort}" ]; then
-    portType='alive'
+    portType='live'
 fi
 
 # Patch and restart sshd
 echo "Patching our sshd config."
 # Allow root logins with keys only
-sed -i "s/#PermitRootLogin yes/PermitRootLogin without-password/" ${ourFile}
+if [ "$(grep "#PermitRootLogin yes" "${ourFile}" )" ]; then
+    echo "Patching PermitRootLogin"
+    sed -i "s/#PermitRootLogin yes/PermitRootLogin without-password/" "${ourFile}"
+    update='true'
+else
+    echo "PermitRootLogin already patched - skipping."
+fi
+
 # Setup our sshd port
 if [ "${portType}" == 'dead' ]; then
+    echo "Appending port to ${ourFile}"
     echo "Port ${sshPort}" >> ${ourFile}
-elif [ "${portType}" == 'live' -a "${sshPort}" != ${livePort} ]; then
-    sed -i "s/Port ${livePort}/Port ${sshPort}/g"
-elif [ "${portType}" == 'live' -a "${sshPort}" == ${livePort} ]; then
-    echo "sshd already configured to port ${livePort}."
+    updated='true'
+elif [ "${portType}" == 'live' -a "${sshPort}" -ne "${livePort}" ]; then
+    echo "Updating port on ${ourFile}"
+    sed -i "s/Port ${livePort}/Port ${sshPort}/g" "${ourFile}"
+    updated='true'
+elif [ "${portType}" == 'live' -a "${sshPort}" -eq "${livePort}" ]; then
+    echo "sshd already configured to port ${livePort} - skipping."
+# If we get here, something is broken. We should never get here.
 else
     echo "Something broke with our port configuration." >&2
+    echo "portType ${portType}" >&2
+    echo "livePort ${livePort}" >&2
+    echo "sshPort ${sshPort}" >&2
     exit 1
 fi
+
 echo "Done patching our sshd config."
 
-/etc/init.d/sshd restart || exit 1
+if [ "${updated}" == 'true' ]; then
+    /etc/init.d/sshd restart || exit 1
+else
+   echo "Nothing changed - not restarting sshd."
+fi
 
 exit 0
