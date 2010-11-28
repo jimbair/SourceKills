034808c5eb491a650d7dbd3c02c04dbde719fa8c
James Bair <james.d.bair@gmail.com>
2010-11-28 15:19:11 -0600

    Added checks in case we botch the password.

--- sys_scripts/deploy/modules/001_root_passwd.sh
+++ sys_scripts/deploy/modules/001_root_passwd.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 # This module will set the root password if given one.
+pwSet='no'
+while [ "${pwSet}" == 'no' ]; do
 
-read -p "Please enter desired root password (press enter to skip): " rootPW
-[ -z "${rootPW}" ] && exit 0
+    read -p "Please enter desired root password (press enter to skip): " rootPW
 
-echo -n "Setting root password..."
+    # If nothing given, exit.
+    [ -z "${rootPW}" ] && exit 0
+
+    # Make sure the user entered the correct password.
+    read -p "You entered '${rootPW}' - is this correct? " answer
+    # Convert to lower case
+    answer="$(echo ${answer} | tr A-Z a-z)"
+    case ${answer} in
+        yes|y)
+            pwSet='yes'
+            ;;
+        no|n)
+            echo "Okay - resetting password. Let's try again."
+            ;;
+        *)
+            echo "${answer} not understood - starting over."
+            ;;
+    esac
+done
+
+# Change our password and exit.
+echo -n "Setting root password to ${rootPW}..."
 echo "root:${rootPW}" | chpasswd || exit 1
 echo 'done.'
 
