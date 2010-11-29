85397c8f1e8d75aaf8ca76ddcbb33c0b1079aa83
James Bair <james.d.bair@gmail.com>
2010-11-28 21:46:01 -0600

    The hunt for python-updater perfection continues. =)

--- sys_scripts/deploy/modules/102_update_world.sh
+++ sys_scripts/deploy/modules/102_update_world.sh
@@ -24,12 +24,29 @@ echo "Finished updating portage."
 echo "Installing Gentoolkit if needed."
 emerge -u gentoolkit
 
+# See Gentoo Bug #347081 http://bugs.gentoo.org/show_bug.cgi?id=347081i
+py3="$(which python3 2>/dev/null)"
+if [ -z "${py3}" ]; then
+    echo -n "Generating our python3 symlink..."
+    eselectOrig="$(eselect python list | grep '*' | cut -d '[' -f 2 | cut -d ']' -f 1)"
+    eselectPy3="$(eselect python list | grep 'python3.1' | cut -d '[' -f 2 | cut -d ']' -f 1)"
+    eselect python set ${eselectPy} || exit 1
+    eselect python set ${eselectOrig} || exit 1
+    echo 'done.'
+else
+    echo "Symlink already generated - skipping."
+fi
+
+# Generally needs run before world update for dev-libs/libxml2-2.7.7
+python-updater || exit 1
+
 # Now update everything!
 echo "Proceeding with updating entire OS."
 # First one can sometimes fail with lots of updates due to order
 emerge -uDN world || emerge -uDN world || exit 1
 
 # Required after upgrades. If not needed, doesn't hurt anything.
+python-updater || exit 1
 perl-cleaner --all || exit 1
 
 # Python should have been updated as part of portage
