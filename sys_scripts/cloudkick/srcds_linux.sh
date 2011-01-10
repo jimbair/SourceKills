2dbfedf4cd3d07b298c3d4fd78b4b81051e57c97
James Bair <james.d.bair@gmail.com>
2011-01-09 19:14:14 -0600

    Adding CloudKick module for checking if srcds_linux is running.

---
+++ sys_scripts/cloudkick/srcds_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This module simply checks if srcds_linux is running
+
+# Find the PID of our SourceDS server(s)
+ourPIDs="$(pidof srcds_linux)"
+
+# Exit accordingly.
+if [ -n "${ourPIDs}" ]; then
+    echo -n "status ok Found $(echo ${ourPIDs} | wc -w) SourceDS instances:"
+    for i in ${ourPIDs}; do
+        echo -n " ${i}"
+    done
+    echo ''
+    # Print our PIDs as integer metrics. May be useful
+    for i in ${ourPIDs}; do
+        echo "metric srcds_linux_pid int ${i}"
+    done
+    exit 0
+else
+    echo "status err No SourceDS instances found."
+    exit 1
+fi
