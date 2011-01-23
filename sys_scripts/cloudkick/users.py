54d212450d018a1b46c70c42c94c9b129eb3d7f1
James Bair <james.d.bair@gmail.com>
2011-01-23 16:41:30 -0600

    Cleaned up a bit - made it modular.

--- sys_scripts/cloudkick/users.py
+++ sys_scripts/cloudkick/users.py
@@ -5,16 +5,45 @@
 import commands
 import sys
 
-users = commands.getoutput('who').strip()
+def getUsersLoggedIn():
+    """
+    Find all users that are logged 
+    into the local system and return
+    either a list or a None object.
+    """
+    
+    # Best way I can find to find users logged in
+    userData = commands.getoutput('who').strip()
+    
+    # "who" returns an empty string if no one is logged in
+    if userData == '':
+        users = None
+    else:
+        # We're going to add each user we find to a list
+        users = []
+        # Split up our string by lines
+        userLines = userData.split('\n')
+        for line in userLines:
+            # Username should be the first item on the line
+            users.append(line.split()[0])
 
-if users == '':
-    sys.stdout.write("status ok No users logged in.\n")
-    sys.exit(0)
-else:
-    users = users.split('\n')
-    sys.stderr.write("status err %d users logged in:" % (len(users),))
-    for user in users:
-        user = user.split()[0]
-        sys.stderr.write(" %s" % (user,))
-    sys.stderr.write('\n')
-    sys.exit(1)
+    return users
+
+# Let's do this
+if __name__ == '__main__':
+
+    # Find our users
+    users = getUsersLoggedIn()
+
+    # Currently only supports zero or anything else.
+    # Should be easy to add support for X number of 
+    # users to be okay, but >X alerting.
+    if users is None:
+        sys.stdout.write("status ok No users logged in.\n")
+        sys.exit(0)
+    else:
+        sys.stderr.write("status err %d users logged in:" % (len(users),))
+        for user in users:
+            sys.stderr.write(" %s" % (user,))
+        sys.stderr.write('\n')
+        sys.exit(1)
